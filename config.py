aaa430e3e2f0818f259c4660dd104f836296b838
Ghanashyamgudela <157698137+Ghanashyamgudela@users.noreply.github.com>
2025-12-04 19:00:12 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+
+MYSQL_HOST = 'localhost'       # your MySQL host, usually localhost
+MYSQL_USER = 'root'            # your MySQL username
+MYSQL_PASSWORD = 'Ghana@1230'            # your MySQL password
+MYSQL_DB = 'student'  # your database name
+MAIL_USER = "ghana19183@gmail.com"
+MAIL_PASS = "jmwe kafu lmdp cmpr"   # Use Gmail App Password (NOT your Gmail login)
+
